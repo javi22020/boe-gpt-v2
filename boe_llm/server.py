5ec2ef1d95b58ffb25fb3eb91c427cf071843acd
javi22020 <90896750+javi22020@users.noreply.github.com>
2024-08-28 23:20:36 +0200

    chore: Add support for loading GGUF models in server.py

--- boe_llm/server.py
+++ boe_llm/server.py
@@ -2,6 +2,7 @@ from llama_cpp.server.app import create_app
 from llama_cpp.server.settings import ModelSettings, ServerSettings
 import uvicorn, os, yaml
 config = yaml.safe_load(open("config.yaml", "r", encoding="utf-8"))
+models = [m for m in os.listdir("model") if m.endswith(".gguf")]
 app = create_app(
     server_settings=ServerSettings(
         host="0.0.0.0",
