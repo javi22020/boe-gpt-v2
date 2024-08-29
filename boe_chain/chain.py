939876e7a7f912b46af84813c4ca7969547554c9
javi22020 <90896750+javi22020@users.noreply.github.com>
2024-08-29 03:07:58 +0200

    chore: Update langchain_openai to use OpenAIEmbeddings in chain.py

--- boe_chain/chain.py
+++ boe_chain/chain.py
@@ -4,7 +4,7 @@ from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.prompts.prompt import PromptTemplate
 from langchain_chroma.vectorstores import Chroma
-from langchain_community.embeddings.localai import LocalAIEmbeddings
+from langchain_openai.embeddings.base import OpenAIEmbeddings
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from chromadb import HttpClient, EmbeddingFunction
 
@@ -17,17 +17,6 @@ def get_history(id: int):
         session[id] = []
     return session[id]
 
-class OpenAIEmbeddings(EmbeddingFunction):
-    def __init__(self) -> None:
-        super().__init__()
-        self.embed = openai.OpenAI(
-            # base_url="http://127.0.0.1:5550",
-            api_key="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"
-        )
-
-    def __call__(self, input: Documents) -> Embeddings:
-        return self.embed.embeddings.create(input=input, model="text-embedding-3-small").data[0].embedding
-
 class BOEGPTChain:
     def __init__(self, model: str) -> None:
         api_key="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"
@@ -36,12 +25,8 @@ class BOEGPTChain:
             model=model,
             api_key=api_key
         )
-        embed_client = openai.OpenAI(
-            base_url="http://127.0.0.1:5550",
-            api_key=api_key
-        )
         self.prompt_docs = PromptTemplate.from_template(open("prompt_docs.md", "r", encoding="utf-8").read())
-        self.chroma = Chroma(client=HttpClient(host="127.0.0.1", port=8000), collection_name="docs", embedding_function=OpenAIEmbeddings(client=embed_client))
+        self.chroma = Chroma(client=HttpClient(host="127.0.0.1", port=8000), collection_name="docs", embedding_function=OpenAIEmbeddings(api_key=api_key))
         self.doc_chain = create_stuff_documents_chain(
             llm=self.llm,
             prompt=self.prompt_docs
